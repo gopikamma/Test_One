8d5b7b85a16c655c1267ed29b5c346bd8e00f919
gopikamma <gopi@gmail.com>
2018-12-20 04:57:08 +0530

    Login Method Updated

---
+++ src/Package2/Class2.java
@@ -0,0 +1,11 @@
+package Package2;
+
+public class Class2 {
+
+	public static void main(String[] args)
+	{
+		System.out.println("Programs Package Class2");
+
+	}
+
+}
---
+++ src/Package2/Class2a.java
@@ -0,0 +1,11 @@
+package Package2;
+
+public class Class2a {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Class2a for Package2 and Programs");
+
+	}
+
+}
